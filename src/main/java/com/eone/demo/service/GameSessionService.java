3d91eaa5dad211f3286580506f7413b7ac7817b4
e1-one <vanya.yarovoy@gmail.com>
2021-01-21 20:17:54 +0200

    make move game logic

--- src/main/java/com/eone/demo/service/GameSessionService.java
+++ src/main/java/com/eone/demo/service/GameSessionService.java
@@ -26,7 +26,22 @@ public class GameSessionService {
     }
 
     public Mono<DieRoll> makeMove(String gameSessionId) {
+        Mono<GameSession> byId = gameSessionRepository.findById(gameSessionId);
+        Mono<DieRoll> dieRoll = randomService.getRandomDice()
+                .flatMap(diceNumber -> {
+                    DieRoll dr = new DieRoll();
+                    dr.setDiceValue(diceNumber);
+                    return dieRollRepository.save(dr);
+                });
 
+        Mono.zip(byId, dieRoll).map(tuple -> {
+            GameSession gs = tuple.getT1();
+            DieRoll newDieRoll = tuple.getT2();
+            SnakeAndLadderGameLogic.changeGameSessionState(gs, newDieRoll);
+            return gs;
+        }).subscribe(gs -> {
+            gameSessionRepository.save(gs).subscribe();
+        });
         return dieRoll;
     }
 }
